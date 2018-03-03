12e361d666dd3275940bfdeebc5cf802b8e4d8a3
João Paulo Maiamaral Fiumari <joaopaulo_mf@hotmail.com>
2018-03-03 18:42:00 -0300

    Primeiro Commit

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeatherChecker.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        [HttpGet("[action]/{city}")]
+        public async Task<IActionResult> City(string city)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new System.Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=19efe6c141405b18271915691812e9fe&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Main.Temp,
+                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        City = rawWeather.Name
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+
+    }
+    public class OpenWeatherResponse
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<WeatherDescription> Weather { get; set; }
+
+        public Main Main { get; set; }
+    }
+    public class WeatherDescription
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿### Weather Checker
+
+- Projeto em Javascript e C# utilizando  Angular 2 e  AspNet Core WEB API 
+
